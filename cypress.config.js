a9d650b60eda6a39ca49f7fec2d281504fbe2ca0
Luciano <lof.fagundes@gmail.com>
2026-03-27 10:32:03 -0300

    configurar ambiente

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {},
   },
-});
+  env: {
+    token: '',
+  },
+  reporter: 'spec',
+  video: false,
+})
