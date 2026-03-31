1ce3a8ef95369c2ca8b7ac046a9a8a2b93fb2358
Luciano <lof.fagundes@gmail.com>
2026-03-31 10:39:50 -0300

    feat: validar schema

---
+++ cypress/support/schemas/carrinho.schema.js
@@ -0,0 +1,25 @@
+const carrinhoSchema = {
+  type: 'object',
+  properties: {
+    _id:            { type: 'string' },
+    produtos: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          idProduto:    { type: 'string' },
+          quantidade:   { type: 'number' },
+          precoUnitario:{ type: 'number' },
+        },
+        required: ['idProduto', 'quantidade', 'precoUnitario'],
+      },
+    },
+    precoTotal:     { type: 'number' },
+    quantidadeTotal:{ type: 'number' },
+    idUsuario:      { type: 'string' },
+  },
+  required: ['_id', 'produtos', 'precoTotal', 'quantidadeTotal', 'idUsuario'],
+  additionalProperties: false,
+}
+
+module.exports = { carrinhoSchema }
