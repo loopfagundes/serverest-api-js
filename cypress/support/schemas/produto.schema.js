b69a84947b8e5dbb7bd432e624ffaf00d69543f6
Luciano <lof.fagundes@gmail.com>
2026-03-31 10:16:32 -0300

    feat: service e schema

---
+++ cypress/support/schemas/produto.schema.js
@@ -0,0 +1,14 @@
+const produtoSchema = {
+  type: 'object',
+  properties: {
+    _id:        { type: 'string' },
+    nome:       { type: 'string' },
+    preco:      { type: 'number' },
+    descricao:  { type: 'string' },
+    quantidade: { type: 'number' },
+  },
+  required: ['_id', 'nome', 'preco', 'descricao', 'quantidade'],
+  additionalProperties: false,
+}
+
+module.exports = { produtoSchema }
