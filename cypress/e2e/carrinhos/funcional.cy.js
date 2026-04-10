4ef49a636073ab02064f6c38a1c5e284bd308a52
Luciano <lof.fagundes@gmail.com>
2026-04-10 10:01:23 -0300

    feat: adiciona as de get, post e del

--- cypress/e2e/carrinhos/funcional.cy.js
+++ cypress/e2e/carrinhos/funcional.cy.js
@@ -1,15 +1,43 @@
+import { deletarUsuario } from "../../support/services/usuarios.service"
 import { criarProduto, deletarProduto } from '../../support/services/produtos.service'
-import { getCarrinhos, criarCarrinho, deletarCarrinho } from '../../support/services/carrinhos.service'
+import { getCarrinhos, getCarrinhoPorId, criarCarrinho, deletarCarrinho, deletarCancelarCompra } from '../../support/services/carrinhos.service'
+
+describe('[GET] Carrinhos - Testes Funcionais', () => {
+
+  before(() => {
+    cy.login('fulano@qa.com', 'teste')
+  })
+
+  it('GET / CT001 - Buscar todos os carrinhos', () => {
+    getCarrinhos(Cypress.env('token')).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.carrinhos).to.be.an('array')
+    })
+  })
+
+  it('GET ID / CT001 - Buscar carrinho por ID', () => {
+    getCarrinhos(Cypress.env('token')).then((response) => {
+      const carrinhoId = response.body.carrinhos[0]._id
+      getCarrinhoPorId(Cypress.env('token'), carrinhoId).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body).to.have.property('_id')
+        expect(response.body.produtos).to.be.an('array')
+      })
+    })
+  })
+
+})
 
 describe('Carrinhos - Testes Funcionais', () => {
 
   let produtoId
 
   before(() => {
-    cy.login('fulano@qa.com', 'teste')
+    cy.loginAdmin()
     cy.fixture('produtos/produto').then((produto) => {
       produto.nome = `Produto Carrinho QA ${Date.now()}`
-      criarProduto(Cypress.env('token'), produto).then((response) => {
+      produto.quantidade = 10
+      criarProduto(Cypress.env('tokenAdmin'), produto).then((response) => {
         expect(response.status).to.eq(201)
         produtoId = response.body._id
       })
@@ -17,52 +45,68 @@ describe('Carrinhos - Testes Funcionais', () => {
   })
 
   after(() => {
-    deletarProduto(Cypress.env('token'), produtoId)
+    deletarCarrinho(Cypress.env('tokenAdmin'))
+    deletarProduto(Cypress.env('tokenAdmin'), produtoId)
+    deletarUsuario(Cypress.env('usuarioAdminId'))
   })
 
-  it('deve listar todos os carrinhos', () => {
-    getCarrinhos(Cypress.env('token')).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body.carrinhos).to.be.an('array')
-    })
-  })
 
-  it('deve criar um carrinho com sucesso', () => {
+  it('POST / CT001 - Adicionar o ID do produto ao carrinho', () => {
     cy.fixture('carrinhos/carrinho').then((carrinho) => {
       carrinho.produtos[0].idProduto = produtoId
-      criarCarrinho(Cypress.env('token'), carrinho).then((response) => {
-        expect(response.status).to.eq(201)
-        expect(response.body.message).to.eq('Cadastro realizado com sucesso')
-      })
-    })
-  })
-
-  it('deve concluir a compra com sucesso', () => {
-    deletarCarrinho(Cypress.env('token')).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body.message).to.eq('Registro excluído com sucesso')
-    })
-  })
-
-  it('não deve criar dois carrinhos para o mesmo usuário', () => {
-    cy.fixture('carrinhos/carrinho').then((carrinho) => {
-      carrinho.produtos[0].idProduto = produtoId
-      criarCarrinho(Cypress.env('token'), carrinho).then(() => {
-        criarCarrinho(Cypress.env('token'), carrinho).then((response) => {
-          expect(response.status).to.eq(400)
-          expect(response.body.message).to.eq('Não é permitido ter mais de 1 carrinho')
-          deletarCarrinho(Cypress.env('token'))
+      cy.fixture('carrinhos/message').then((message) => {
+        criarCarrinho(Cypress.env('tokenAdmin'), carrinho).then((response) => {
+          expect(response.status).to.eq(201)
+          expect(response.body.message).to.eq(message.adicionaProdutoAoCarrinhoComSucesso)
         })
       })
     })
   })
 
-  it('não deve criar carrinho sem token', () => {
+  it('DELETE / CT001 - Excluir um carrinho cadastrado como concluir a compra', () => {
+    cy.fixture('carrinhos/message').then((message) => {
+      deletarCarrinho(Cypress.env('tokenAdmin')).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.message).to.eq(message.carrinhoExcluidoComSucesso)
+      })
+    })
+  })
+
+})
+
+describe('[DELETE] Carrinhos - Testes Funcionais', () => {
+
+  let produtoId
+
+  before(() => {
+    cy.loginAdmin()
+    cy.fixture('produtos/produto').then((produto) => {
+      produto.nome = `Produto Carrinho QA ${Date.now()}`
+      produto.quantidade = 5
+      criarProduto(Cypress.env('tokenAdmin'), produto).then((response) => {
+        expect(response.status).to.eq(201)
+        produtoId = response.body._id
+      })
+    })
     cy.fixture('carrinhos/carrinho').then((carrinho) => {
       carrinho.produtos[0].idProduto = produtoId
-      criarCarrinho('', carrinho).then((response) => {
-        expect(response.status).to.eq(401)
-        expect(response.body.message).to.eq('Token de acesso ausente, inválido, expirado ou usuário do token não existe mais')
+      criarCarrinho(Cypress.env('tokenAdmin'), carrinho).then((response) => {
+        expect(response.status).to.eq(201)
+      })
+    })
+  })
+
+
+  after(() => {
+    deletarProduto(Cypress.env('tokenAdmin'), produtoId)
+    deletarUsuario(Cypress.env('usuarioAdminId'))
+  })
+
+  it('DELETE / CT001 - Excluir um carrinho cadastrado como desistência da compra', () => {
+    cy.fixture('carrinhos/message').then((message) => {
+      deletarCancelarCompra(Cypress.env('tokenAdmin')).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.message).to.eq(message.produtosReabastecidos)
       })
     })
   })
