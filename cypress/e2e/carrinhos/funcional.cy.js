271858a8d0d1ce31657dedf4aef9029ae09ad089
Luciano <lof.fagundes@gmail.com>
2026-04-10 10:13:09 -0300

    refactor: ajusta fazer login antes de add carrinhos

--- cypress/e2e/carrinhos/funcional.cy.js
+++ cypress/e2e/carrinhos/funcional.cy.js
@@ -4,21 +4,43 @@ import { getCarrinhos, getCarrinhoPorId, criarCarrinho, deletarCarrinho, deletar
 
 describe('[GET] Carrinhos - Testes Funcionais', () => {
 
+  let produtoId
+
   before(() => {
-    cy.login('fulano@qa.com', 'teste')
+    cy.loginAdmin()
+    cy.fixture('produtos/produto').then((produto) => {
+      produto.nome = `Produto Carrinho QA ${Date.now()}`
+      produto.quantidade = 5
+      criarProduto(Cypress.env('tokenAdmin'), produto).then((response) => {
+        expect(response.status).to.eq(201)
+        produtoId = response.body._id
+      })
+    })
+    cy.fixture('carrinhos/carrinho').then((carrinho) => {
+      carrinho.produtos[0].idProduto = produtoId
+      criarCarrinho(Cypress.env('tokenAdmin'), carrinho).then((response) => {
+        expect(response.status).to.eq(201)
+      })
+    })
+  })
+
+  after(() => {
+    deletarCarrinho(Cypress.env('tokenAdmin'))
+    deletarProduto(Cypress.env('tokenAdmin'), produtoId)
+    deletarUsuario(Cypress.env('usuarioAdminId'))
   })
 
   it('GET / CT001 - Buscar todos os carrinhos', () => {
-    getCarrinhos(Cypress.env('token')).then((response) => {
+    getCarrinhos(Cypress.env('tokenAdmin')).then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body.carrinhos).to.be.an('array')
     })
   })
 
   it('GET ID / CT001 - Buscar carrinho por ID', () => {
-    getCarrinhos(Cypress.env('token')).then((response) => {
+    getCarrinhos(Cypress.env('tokenAdmin')).then((response) => {
       const carrinhoId = response.body.carrinhos[0]._id
-      getCarrinhoPorId(Cypress.env('token'), carrinhoId).then((response) => {
+      getCarrinhoPorId(Cypress.env('tokenAdmin'), carrinhoId).then((response) => {
         expect(response.status).to.eq(200)
         expect(response.body).to.have.property('_id')
         expect(response.body.produtos).to.be.an('array')
